c11cf09bd48c55fa55cad87fc9cb5aab58a86f6a
AB008GA <AB008GA@IN-1HS8TQ2-10.pbi.global.pvt>
2019-05-22 19:40:54 +0530

    Changes for Logger of the Application

--- Point_Pb/src/test/java/Page_Object/Add_Point_Customer_Object.java
+++ Point_Pb/src/test/java/Page_Object/Add_Point_Customer_Object.java
@@ -26,6 +26,11 @@ public class Add_Point_Customer_Object extends BaseClass{
 	@FindBy(how=How.XPATH, using="//button[@class='ui primary button ng-isolate-scope']")
 	public static WebElement Add_Customer;
 	
+	@FindBy(how=How.XPATH, using="//input[@placeholder='Customer BPN']")
+	public static WebElement Customer_BPN;
+	
+	
+	
 	@FindBy(how=How.XPATH, using="//select[@name='customerType']")
 	public static WebElement Customer_Type;
 	
@@ -35,11 +40,26 @@ public class Add_Point_Customer_Object extends BaseClass{
 	@FindBy(how=How.XPATH, using="//select[@name='industry']")
 	public static WebElement Customer_Industry;
 	
+	@FindBy(how=How.XPATH, using="//select[@name='acquisition']")
+	public static WebElement Acquisition_Method;
+	
+	@FindBy(how=How.XPATH, using="//select[@name='terms']")
+	public static WebElement Payment_Terms;
+	
+	@FindBy(how=How.XPATH, using="//input[@placeholder='Street Address']")
+	public static WebElement Mailing_Address;
+	
+	@FindBy(how=How.XPATH, using="//input[@placeholder='City']")
+	public static WebElement Mailing_City;
+	
+	@FindBy(how=How.XPATH, using="//input[@class='search']")
+	public static WebElement Mailing_State;
+	
+	@FindBy(how=How.XPATH, using="//input[@placeholder='ZIP Code']")
+	public static WebElement Mailing_Zip_code;
+	
+	@FindBy(how=How.XPATH, using="//button[contains(text(),'Save')]")
+	public static WebElement Save_button;
 	
 	
-	
-	
-	
-	
-
 }
--- Point_Pb/src/test/java/Step_Defination/Add_Point_Customer_Step.java
+++ Point_Pb/src/test/java/Step_Defination/Add_Point_Customer_Step.java
@@ -31,20 +31,20 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class Add_Point_Customer_Step {
-	static final Logger logger = Logger.getLogger(Add_Point_Customer_Step.class);
+	Logger logger=Logger.getLogger("Add_Point_Customer_Step");
 	public WebDriver driver;
 	
 	@Given("^Open browser for Add Customer in Points application$")
 	public void open_browser_for_Add_Customer_in_Points_application() throws Throwable {
 		driver = Hooks.driver;
-		BasicConfigurator.configure();
+		
 		logger.info("Fetching URL and Opening the Url");
 		
 	}
 
 	@When("^NewUser select \"([^\"]*)\" for Add Customer in Points application$")
 	public void newuser_select_for_Add_Customer_in_Points_application(String center) throws Throwable {
-		BasicConfigurator.configure();
+		//BasicConfigurator.configure();
 		
 		//Initialization
 		PageFactory.initElements(driver, Add_Point_Customer_Object.class);
@@ -57,7 +57,7 @@ public class Add_Point_Customer_Step {
 		Thread.sleep(2000);
 		element1.click();
 		//Center Selection
-		//Add_Count_Object.Select_Center_Click.click();
+		
 		Select Centers = new Select(Add_Point_Customer_Object.Select_Center_List);
 		
 			
@@ -70,8 +70,9 @@ public class Add_Point_Customer_Step {
 	            break;
 	        }
 	    }
-		
-	
+	   
+	    logger.info(center+" "+"is the Center Selected by the User");
+	    
 		WebDriverWait wait2 = new WebDriverWait(driver, 50);
 		WebElement element2 = wait2
 				.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='text flex center-both'][contains(text(),'Customers')]")));
@@ -95,15 +96,19 @@ public class Add_Point_Customer_Step {
 		}
 		
 	
-	@When("^Customer click on Add Customer and Enter Customer with details \"(.*?)\" , \"(.*?)\" , \"(.*?)\" , \"(.*?)\" , \"(.*?)\" and \"(.*?)\"$")
-	public void Add_Customer_and_Enter(String Customer_Name, String Customer_Type,String Industry, String arg4, String arg5, String arg6) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-		
+	@When("^Customer click on Add Customer and Enter Customer with details \"(.*?)\" ,\"(.*?)\", \"(.*?)\" , \"(.*?)\" , \"(.*?)\" , \"(.*?)\" and \"(.*?)\"$")
+	public void Add_Customer_and_Enter(String Customer_Name, String Customer_BPN, String Customer_Type,String Industry, String Acquisition_Method, String Payment_Terms, String Start_Date) throws Throwable {
+	   		
 		Add_Point_Customer_Object.Customer_Name.sendKeys(Customer_Name);
+		
+		Add_Point_Customer_Object.Customer_BPN.sendKeys(Customer_BPN);
 	   
 		((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", Add_Point_Customer_Object.Customer_Type);
 		
 		Thread.sleep(2000);
+		
+		//Customer_Type
+		
 		Select Counttype = new Select(Add_Point_Customer_Object.Customer_Type);
 		
 		List<WebElement> Counttype_list = Counttype.getOptions();
@@ -115,8 +120,13 @@ public class Add_Point_Customer_Step {
 	            break;
 	        }
 	    }
+	    logger.info(Customer_Type+" "+"is the Customer Type selected by the User");
+	    
+	    //Industry type
 		
 		Select Industry_Type = new Select(Add_Point_Customer_Object.Customer_Industry);
+		
+		 
 		
 		List<WebElement> allOptions = Industry_Type.getOptions();
 		
@@ -128,24 +138,99 @@ public class Add_Point_Customer_Step {
 	        }
 	    }
 		
+	    logger.info(Industry+" "+"is the Industry Selected by the User");
+	    
+		//Acquisition Method
+	    
+	    Select Acquisition = new Select(Add_Point_Customer_Object.Acquisition_Method);
 		
+		List<WebElement> acquisition_list = Acquisition.getOptions();
 		
+		// Loop one by one
+	    for (int j = 0; j < acquisition_list.size(); j++) {	        
+	        if(acquisition_list.get(j).getText().equals(Acquisition_Method)) {
+	        	acquisition_list.get(j).click();
+	            break;
+	        }
+	    }
+	    
+	    logger.info(Acquisition_Method+" "+"is the Acquisition Method Selected by the User");
+	    
+	    //Payment Terms
+	    
+	    Select Payment = new Select(Add_Point_Customer_Object.Payment_Terms);
+		
+		List<WebElement> Payment_list = Payment.getOptions();
+		
+		// Loop one by one
+	    for (int j = 0; j < Payment_list.size(); j++) {	        
+	        if(Payment_list.get(j).getText().equals(Payment_Terms)) {
+	        	Payment_list.get(j).click();
+	            break;
+	        }
+	    }
+	    
+	    logger.info(Payment_Terms+" "+"is the Payment Terms Selected by the User");
 		
 	}
 	
 	@When("^Customer enters PrimaryMailingAddress with details \"(.*?)\" and \"(.*?)\" and \"(.*?)\" and \"(.*?)\"$")
-	public void customer_enters_PrimaryMailingAddress_with_details_and_and_and(String arg1, String arg2, String arg3, String arg4) throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
+	public void customer_enters_PrimaryMailingAddress_with_details_and_and_and(String Address, String City, String State, String Zipcode) throws Throwable {
+	    
+		//Address
+		Add_Point_Customer_Object.Mailing_Address.sendKeys(Address);
+		
+		logger.info(Address+" "+"is the Address provided by the User");
+		
+		//City
+		Add_Point_Customer_Object.Mailing_City.sendKeys(City);
+		
+		logger.info(City+" "+"is the City provided by the User");
+		
+		((JavascriptExecutor)driver).executeScript("arguments[0].scrollIntoView();", Add_Point_Customer_Object.Mailing_State);
+	    //State
+	   
+		Add_Point_Customer_Object.Mailing_State.sendKeys(State);
+		
+		logger.info(State+" "+"is the State provided by the User");
+		
+		//ZipCode
+		Add_Point_Customer_Object.Mailing_Zip_code.sendKeys(Zipcode);
+		
+		logger.info(Zipcode+" "+"is the Zipcode provided by the User");
+		
+		Thread.sleep(2000);
+		
+	    Add_Point_Customer_Object.Save_button.click();
+	    
+	    
+
+		
 	}
-
-	@Then("^Message displayed Login Successful for Add Customer in Points application$")
-	public void message_displayed_Login_Successful_for_Add_Customer_in_Points_application() throws Throwable {
-	    // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
-	}
-
-
 	
+	
+	@Then("^Message displayed Login Successful for Add \"(.*?)\" in Points application$")
+	public void message_displayed_Login_Successful_for_Add_Customer_in_Points_application(String Customer_Name) throws Throwable {
+		WebDriverWait wait = new WebDriverWait(driver, 50);
+		WebElement element = wait
+				.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='toast-message']")));
+		Thread.sleep(2000);
+		
+		String Text_value= element.getText();
+		System.out.println("Text Value is-->"+Text_value);
+		
+		try {
+		Assert.assertEquals(Customer_Name+ " " +"was created", Text_value);
+		logger.info("Assertion Passed");
+		}
+		catch (AssertionError e) {
+		logger.info("Assertion Failed");
+		       throw e;
+		     
+	}
+
+
+
+	}	
 
 }
